cc7575c4fb1bd1cf7d48347a9405557098b84f0d
Thiago Barcelos <thiagolbf@hotmail.com>
2022-12-05 20:38:54 -0300

    feature for pagination

--- src/store/modules/popularmovies/thunks.ts
+++ src/store/modules/popularmovies/thunks.ts
@@ -9,7 +9,7 @@ export const getPopularMoviesThunk = (page: number) => (dispatch: Dispatch) => {
       `movie/popular?api_key=41c0ef2437f7b84f0296214347b788c7&language=pt-BR&page=${page}`
     )
     .then((res) => {
-      dispatch(getPopularMovies(res.data.results));
+      dispatch(getPopularMovies(res.data));
     })
     .catch((err) => {
       console.log(err);
